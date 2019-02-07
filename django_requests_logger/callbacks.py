21aa07894c26f43cf67ac2d55910de1f2e2d3cfe
Miguel Ferrer <ingferrermiguel@gmail.com>
2019-02-07 15:24:35 -0500

    Add data_masking arg Add exception handling

--- django_requests_logger/callbacks.py
+++ django_requests_logger/callbacks.py
@@ -1,9 +1,11 @@
 import json
 
+from urllib.parse import parse_qs
+
 from django_requests_logger.models import RequestLog
 
 
-def logger(response, *args, **kwargs):
+def logger(response, data_masking=None, *args, **kwargs):
     if 'application/json' in response.headers['Content-Type']:
         content = response.json()
     else:
@@ -19,8 +21,21 @@ def logger(response, *args, **kwargs):
         'response_headers': dict(response.headers),
     }
     if response.request.body:
-        body = json.loads(response.request.body.decode())
-        body = {k: v if not k.startswith('credit_card') else '*****' for k, v in body.items()}
+        try:
+            body = json.loads(response.request.body.decode())
+        except Exception:
+            try:
+                body = parse_qs(response.request.body)
+            except Exception:
+                body = response.request.body
+
+        if data_masking and isinstance(body, dict):
+            body = masking(body, data_masking)
+
         data['body'] = body
 
     RequestLog.objects.create(**data)
+
+
+def masking(body, data_masking):
+    return {k: '*****' if any((d in k for d in data_masking)) else v for k, v in body.items()}
