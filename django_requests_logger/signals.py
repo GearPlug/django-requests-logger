1851c745975e9dee6df84778354af20289d819e1
mabouchama <mabouchama.itpeople@emaar.ae>
2022-02-17 09:45:27 +0400

    Update signals for django 4 with backwards compatibility

--- django_requests_logger/signals.py
+++ django_requests_logger/signals.py
@@ -1,4 +1,9 @@
 from django.dispatch import Signal
+from django.utils import version
 
-response_is_ok = Signal(providing_args=['instance'])
-response_is_not_ok = Signal(providing_args=['instance'])
+if version.get_version() < '4.0.0':
+    response_is_ok = Signal(providing_args=['instance'])
+    response_is_not_ok = Signal(providing_args=['instance'])
+else:
+    response_is_ok = Signal()
+    response_is_not_ok = Signal()
