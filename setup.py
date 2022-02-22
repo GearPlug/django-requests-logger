9994d5effb5fdfeddf572b2b3a3de8a748f5fda8
Miguel Ferrer <ingferrermiguel@gmail.com>
2022-02-22 08:26:43 -0500

    Bump ver. 0.1.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="django-requests-logger",
-    version="0.1.6",
+    version="0.1.7",
     include_package_data=True,
     license="MIT",
     description="A django integration for requests.",
